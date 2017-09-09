8254f15cba9a8cf3fed6c49fb422988b6e3d9310
Andres Root <info@andresroot.co>
2017-09-09 15:47:24 -0500

    working on variable tensor

--- api/models.py
+++ api/models.py
@@ -2,29 +2,58 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Passenger(models.Model):
-    user = models.OneToOneField(User)
-    active = models.BooleanField(default=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
-
-
 class Package(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
     origin = models.CharField(max_length=255, null=True, blank=True)
     destination = models.CharField(max_length=255, null=True, blank=True)
     cost = models.CharField(max_length=255, null=True, blank=True)
-    description = models.CharField(max_length=255, null=True, blank=True)
+    departure_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    discount = models.CharField(max_length=255, null=True, blank=True)
+    segment = models.CharField(max_length=255, null=True, blank=True)
     active = models.BooleanField(default=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
 
-class Ticket(models.Model):
-    code = models.CharField(max_length=255, null=True, blank=True)
-    cabin = models.CharField(max_length=255, null=True, blank=True)
-    cost = models.CharField(max_length=255, null=True, blank=True)
-    passenger = models.CharField(max_length=255, null=True, blank=True)
-    package = models.CharField(max_length=255, null=True, blank=True)
-    active = models.BooleanField(default=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
+# class Passenger(models.Model):
+#     user = models.OneToOneField(User)
+#     active = models.BooleanField(default=True)
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     date_updated = models.DateTimeField(auto_now=True)
+
+
+# class Ticket(models.Model):
+#     code = models.CharField(max_length=255, null=True, blank=True)
+#     origin = models.CharField(max_length=255, null=True, blank=True)
+#     destination = models.CharField(max_length=255, null=True, blank=True)
+#     cabin = models.CharField(max_length=255, null=True, blank=True)
+#     cost = models.CharField(max_length=255, null=True, blank=True)
+#     passenger = models.CharField(max_length=255, null=True, blank=True)
+#     package = models.CharField(max_length=255, null=True, blank=True)
+#     active = models.BooleanField(default=True)
+#     departure_date = models.DateTimeField()
+#     return_date = models.DateTimeField()
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     date_updated = models.DateTimeField(auto_now=True)
+
+
+# class Segment(models.Model):
+#     name = models.CharField(max_length=255, null=True, blank=True)
+#     description = models.TextField(blank=True, null=True)
+#     packages = models.CharField(max_length=255, null=True, blank=True)
+#     active = models.BooleanField(default=True)
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     date_updated = models.DateTimeField(auto_now=True)
+
+
+# class Preferences(models.Model):
+#     passenger = models.CharField(max_length=255, null=True, blank=True)
+#     name = models.CharField(max_length=255, null=True, blank=True)
+#     tags = models.TextField(blank=True, null=True)
+#     segment = models.TextField(blank=True, null=True)
+#     passenger = models.CharField(max_length=255, null=True, blank=True)
+#     active = models.BooleanField(default=True)
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     date_updated = models.DateTimeField(auto_now=True)
