221303db58e868eeeba23c15f0d8d2d4d02923c3
Andres Root <info@andresroot.co>
2017-09-09 16:32:31 -0500

    Working on API

--- api/views.py
+++ api/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import numpy as np
 
 
+def softmax(L):
+    values = []
+    for n in L:
+        s = np.exp(n) / sum(map(lambda x: np.exp(x), L))
+        values.append(s)
+    return values
+
+
+# Views
 def index(request):
     response = {'200': 'ok'}
     return JsonResponse(response)
