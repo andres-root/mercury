a520c6acf2d23eb889296ecd2dbda7617ee1725a
Andres Root <info@andresroot.co>
2017-09-09 21:27:39 -0500

    Working on API

--- testpy2.py
+++ testpy2.py
@@ -1,4 +1,4 @@
-import urllib.request
+import urllib2
 import base64
 import suds
 import suds.transport
@@ -10,7 +10,8 @@ logging.getLogger('suds.transport').setLevel(logging.DEBUG)
 logging.getLogger('suds.xsd.schema').setLevel(logging.DEBUG)
 logging.getLogger('suds.wsdl').setLevel(logging.DEBUG)
 
-class HTTPSudsPreprocessor(urllib.request.BaseHandler):
+
+class HTTPSudsPreprocessor(urllib2.BaseHandler):
 
     def http_request(self, req):
         message = \
@@ -56,8 +57,8 @@ class HTTPSudsPreprocessor(urllib.request.BaseHandler):
         # user = 'Universal API/uAPI5632685783-282e0d4d'
         # password = 'xS!7K6d%3?'
         # auth = base64.b64encode('Universal API/uAPI2514620686-0edbb8e4:D54HWfck9nRZNPbXmpzCGwc95')
-        # auth = base64.b64encode(b'Universal API/uAPI5632685783-282e0d4d:xS!7K6d%3?')
-        # print(auth.decode('utf-8'))
+        auth = base64.b64encode(b'Universal API/uAPI5632685783-282e0d4d:xS!7K6d%3?')
+        print(auth.decode('utf-8'))
         req.add_header('Accept-Encoding', 'gzip,deflate')
         req.add_header('Content-Type', 'text/xml;charset=UTF-8')
         req.add_header('Cache-Control', 'no-cache')
@@ -65,19 +66,18 @@ class HTTPSudsPreprocessor(urllib.request.BaseHandler):
         req.add_header('SOAPAction', "http://localhost:8080/kestrel/AirService")
         req.add_header('action', "http://localhost:8080/kestrel/AirService")
         # req.add_header('Authorization', 'Basic %s' % (auth.decode('utf-8')))
-        # req.add_header('Authorization', 'Basic VW5pdmVyc2FsIEFQSS91QVBJNTYzMjY4NTc4My0yODJlMGQ0ZDp4UyE3SzZkJTM/')
+        req.add_header('Authorization', 'Basic VW5pdmVyc2FsIEFQSS91QVBJNTYzMjY4NTc4My0yODJlMGQ0ZDp4UyE3SzZkJTM/')
         return req
 
     https_request = http_request
 
 
 URL = "https://americas.universal-api.travelport.com/B2BGateway/connect/uAPI/AirService"
-wsdl = "https://americas.universal-api.travelport.com/B2BGateway/connect/uAPI/AirService?wsdl"
 https = suds.transport.https.HttpTransport()
-opener = urllib.request.build_opener(HTTPSudsPreprocessor)
+opener = urllib2.build_opener(HTTPSudsPreprocessor)
 https.urlopener = opener
 try:
-    suds.client.Client(wsdl, location=URL, transport=https)
+    suds.client.Client(URL, transport=https)
 except Exception as e:
     print(e)
 
